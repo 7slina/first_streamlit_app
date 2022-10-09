9d85b0e010790f6ae672373ae4925b84655c98b3
learn slina <115324022+7slina@users.noreply.github.com>
2022-10-08 20:47:30 -0500

    Update streamlit_app.py
    added user friendly

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,9 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # keep the json data in dataframe 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # kepp the data clean
