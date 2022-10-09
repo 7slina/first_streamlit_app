73990ed0f77f69b42db127e0f0d5c6c8afcfb9de
learn slina <115324022+7slina@users.noreply.github.com>
2022-10-08 20:41:05 -0500

    Update streamlit_app.py
    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# keep the json data in dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# kepp the data clean
+streamlit.dataframe(fruityvice_normalized)
